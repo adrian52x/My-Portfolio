d8614869d88dee3f0ada402759e6523fe81b2d52
Enachi Adrian <enachi.adrian52@gmail.com>
2024-11-03 23:55:50 +0100

    update

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -20,30 +20,30 @@ export const projects: ProjectParams[] = [
     },
     {
         id: "2",
-        title: "Node Download",
-        mainImage: "/images/node-download.jpg",
-        images: ["/images/node-download.jpg", "/images/node-download.jpg"],
+        title: "My first E-Commerce",
+        mainImage: "/images/e-com/1.png",
+        images: ["/images/e-com/1.png", "/images/e-com/2.png", "/images/e-com/3.png", "/images/e-com/4.png", "/images/e-com/5.png"],
         technologies: ["Node.js", "Database"]
     },
     {
         id: "3",
-        title: "NPM Veri",
-        mainImage: "/images/npm-veri.jpg",
-        images: ["/images/npm-veri.jpg", "/images/npm-veri.jpg"],
+        title: "Simple Pokemon App",
+        mainImage: "/images/pokemonApp/1.png",
+        images: ["/images/pokemonApp/1.png", "/images/pokemonApp/2.png"],
         technologies: ["React", "Tailwind"]
     },
     {
         id: "4",
-        title: "Manage App",
-        mainImage: "/images/manage-app.png",
-        images: ["/images/manage-app.png", "/images/manage-app.png"],
+        title: "idk App",
+        mainImage: "/images/node-download.jpg",
+        images: ["/images/node-download.jpg"],
         technologies: ["React", "Node.js", "Database"]
     },
     {
         id: "5",
         title: "Floor Plan (AGC Biologics)",
         mainImage: "/images/floorplan/floorplan-main.jpg",
-        images: ["/images/floorplan/floorplan-dev-steps.jpg", "/images/floorplan/vat83b-2.jpg"],
+        images: ["/images/floorplan/floorplan-dev-steps.jpg", "/images/floorplan/vat83b-2.jpg", "/images/floorplan/vat83b-edit-room.jpg", "/images/floorplan/vat83b-search.jpg"],
         video: "/videos/floor-plan.mp4",
         technologies: ["React", "Tailwind"]
     },
@@ -56,9 +56,9 @@ export const projects: ProjectParams[] = [
     },
     {
         id: "7",
-        title: "Travel",
-        mainImage: "/images/travel.jpg",
-        images: ["/images/travel.jpg", "/images/travel.jpg"],
+        title: "Parking Finder App (Concept)",
+        mainImage: "/images/ParkingFinderApp/OAK-home.jpg",
+        images: ["/images/ParkingFinderApp/OAK-home.jpg", "/images/ParkingFinderApp/OAK-map.jpg", "/images/ParkingFinderApp/OAK-map2.jpg", "/images/ParkingFinderApp/OAK-parkingSlots.jpg", "/images/ParkingFinderApp/OAK-profile.jpg"],
         technologies: ["React", "Tailwind"]
     },
     {
@@ -70,9 +70,9 @@ export const projects: ProjectParams[] = [
     },
     {
         id: "9",
-        title: "Manage App",
-        mainImage: "/images/manage-app.png",
-        images: ["/images/manage-app.png", "/images/manage-app.png"],
+        title: "idk App",
+        mainImage: "/images/texteditor.jpg",
+        images: ["/images/texteditor.jpg"],
         technologies: ["React", "Node.js", "Tailwind", "Database"]
     },
 ];
